8c9964be0550a1a85ca7da0b370263abfff8eab9
shama <shamashaik.eee.rymec@gmail.com>
2026-01-17 16:55:19 +0530

    new

--- backend/cost_estimator.py
+++ backend/cost_estimator.py
@@ -1,9 +1,21 @@
 def estimate_cost(services):
-    cost = {
-        "EC2": services["EC2"] * 20,      # approx $20/month per instance
-        "S3": services["S3"] * 2,
-        "RDS": services["RDS"] * 30,
-        "Lambda": services["Lambda"] * 5
+    cost = {}
+    total = 0
+
+    pricing = {
+        "EC2": 20,
+        "S3": 2,
+        "RDS": 30,
+        "Lambda": 5
     }
-    total = sum(cost.values())
+
+    for service, price in pricing.items():
+        value = services.get(service)
+
+        if isinstance(value, int):
+            cost[service] = value * price
+            total += cost[service]
+        else:
+            cost[service] = "Not available"
+
     return cost, total
